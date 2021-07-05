d9f4c399a5004d304cd74a842417d66d7269946f
lego <lego@gmail.com>
2021-07-05 22:03:42 +0900

    Updates

---
+++ precache-manifest.cb7eab1e29695d3c6d8e6187c68cc2a3.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f992561fad25fe66bca08c2da3653337",
+    "url": "/burger/index.html"
+  },
+  {
+    "revision": "c27c62253d1a52fae93b",
+    "url": "/burger/static/css/3.b093e8d9.chunk.css"
+  },
+  {
+    "revision": "38075e877689af6d1dc8",
+    "url": "/burger/static/css/4.f3469420.chunk.css"
+  },
+  {
+    "revision": "ea104b62bf84ea5df19d",
+    "url": "/burger/static/css/5.0c617de9.chunk.css"
+  },
+  {
+    "revision": "b5929bb158eeb5eeac34",
+    "url": "/burger/static/css/main.7cf4843e.chunk.css"
+  },
+  {
+    "revision": "3e7ebfc14434cb593bec",
+    "url": "/burger/static/js/2.cdb3a79a.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/burger/static/js/2.cdb3a79a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c27c62253d1a52fae93b",
+    "url": "/burger/static/js/3.5c9d4f1c.chunk.js"
+  },
+  {
+    "revision": "38075e877689af6d1dc8",
+    "url": "/burger/static/js/4.25703d1a.chunk.js"
+  },
+  {
+    "revision": "ea104b62bf84ea5df19d",
+    "url": "/burger/static/js/5.594f32e5.chunk.js"
+  },
+  {
+    "revision": "b5929bb158eeb5eeac34",
+    "url": "/burger/static/js/main.480414c9.chunk.js"
+  },
+  {
+    "revision": "f28c0706d00fd76c1516",
+    "url": "/burger/static/js/runtime-main.625bdd79.js"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/burger/static/media/burger-logo.b8503d26.png"
+  }
+]);
